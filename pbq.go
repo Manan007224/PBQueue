840dd56017fdc0f0a0481b9855f1d7140c4af9ad
Manan Maniyar <maniyarmanan1996@gmail.com>
2019-06-18 16:51:29 -0700

    resolved a few errors

--- pbq.go
+++ pbq.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"net/http"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	// "encoding/json"
+	// "fmt"
+	// "io/ioutil"
 	"log"
+	"sync"
 	"github.com/gorilla/websocket"
+	"github.com/satori/go.uuid"
 )
 
 var (
@@ -21,24 +23,25 @@ var (
 
 type Topic struct {
 	// the clients that have subscribed to this topic
-	clientQueue map[connId] chan []byte
+	clientQueue map[string] chan []byte
 
 	topic 	string
 	mtx		sync.RWMutex
 }
 
-func createTopic (topic string) error {
+func createTopic (topic string) {
 	t := &Topic {
 		clientQueue: make(map[string]chan []byte),
 		topic: topic,
 		mtx: sync.RWMutex{},
 	}
-	http.HandleFunc("/topic/"+topic, topicHandler)
+	handler := topicHandler(t)
+	http.HandleFunc("/topic/"+topic, handler)
 }
 
 func topicHandler (t *Topic) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.upgrade(w, r, nil)
+		conn, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
 			log.Println("websocket connection failed")
 			http.Error(w, "Couldn't open ws connection",http.StatusBadRequest)
@@ -46,12 +49,13 @@ func topicHandler (t *Topic) func(w http.ResponseWriter, r *http.Request) {
 		}
 
 		t.mtx.Lock()
-		ch, ok := t.clientQueue[connID(uuid.NewV4.string())]
+		id := uuid.Must(uuid.NewV4()).String()
+		ch, ok := t.clientQueue[id]
 		if !ok {
 			ch = make(chan []byte)
-			clientQueue[c.id] = ch
+			t.clientQueue[id] = ch
 		}
-		t.mtx.UnLock()
+		t.mtx.Unlock()
 			
 		for {
 			select {
@@ -66,15 +70,18 @@ func topicHandler (t *Topic) func(w http.ResponseWriter, r *http.Request) {
 }
 
 // send the message to all the subscribers
-func (t *Topic) Publish (msg []byte) error {
+func (t *Topic) Publish (payload []byte) error {
 	go func() {
-		for _, q := range t.clientQueue{
+		for _, ch := range t.clientQueue{
 			select {
-				case: q.ch <- payload
+				case ch <- payload:
+				default:
 			}
-		}()
-	}
+		}
+	}()
+	return nil
 } 
 
 func main () {
+	http.ListenAndServe(":3000", nil)
 }
