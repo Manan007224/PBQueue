61936859e6785fb06971803c532ecd977b402b11
Manan Maniyar <maniyarmanan1996@gmail.com>
2019-06-19 19:10:46 -0700

    updated example

--- example/pub.go
+++ example/pub.go
@@ -4,17 +4,41 @@ import (
 	"log"
 	"time"
 	"../client"
+	"fmt"
+)
+
+
+var (
+	c = client.New()
 )
 
 func main() {
 
-	c := client.New()
 	tick := time.NewTicker(time.Second)
+
+	go publish()
 
 	for _ = range tick.C {
 		if err := c.Publish("foo", []byte(`bar`)); err != nil {
 			log.Println(err)
 			break
+		}
+	}
+}
+
+func publish() {
+	ch, err := c.Subscribe("foo")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer c.Unsubscribe(ch)
+
+	for i := 0; i < 10; i++ {
+		select {
+		case e := <-ch:
+			log.Println(string(e))
+			fmt.Println(string(e))
 		}
 	}
 }
